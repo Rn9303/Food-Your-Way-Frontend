dcc1d8a6405cf274fda982cdc23272e7c11a3d3c
bradyawn <38081482+bradyawn@users.noreply.github.com>
2023-03-12 14:56:29 +0100

    Separate homescreen from app js (p1)
    Why are we doing this?  Becuase it makes it way easier to code hoemscreen as a functional component and fit navigation with manging app states, etc, etc.  It's going to become a huge mess if we keep this in appjs

---
+++ flask_react/src/HomeScreen.js
@@ -0,0 +1,91 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from './Dashboard'
+import Register from './Register'
+import SearchRecipes from './SearchRecipes'
+import AddRecipes from './AddRecipes'
+import SavedRecipes from './SavedRecipes'
+import SuggestedRecipes from './SuggestedRecipes'
+import Register_header from './Register_header'
+
+import Preferences from './Preferences'
+
+
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  Button,
+  View,
+  SafeAreaView,
+  Text,
+  Alert,
+} from 'react-native';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const HomeScreen = ({navigation}) => {
+  return (
+    <div className="App">
+      <header className="App-header">
+	
+    	<View style={[{ width: "19%", marginTop: "-16%", alignSelf: 'flex-end', flexDirection: "row" }]}>
+		<Button
+  		onPress={() => navigation.navigate('Logout', {name: 'TEMP_USERNAME'})}
+  		title="Register/Login"
+  		accessibilityLabel="Register"/>
+	</View>	
+
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <p>
+          Food Your Way
+        </p>
+
+    <View style={[{ width: "40%", margin: 20 }]}>
+		<Button
+  		onPress={() => navigation.navigate('Dashboard', {name: 'TEMP_USERNAME'})}
+  		title="Dashboard"
+  		color="#50afff"
+  		accessibilityLabel="Dashboard"/>
+	</View>
+
+	<View style={[{ width: "40%", margin: 20 }]}>
+		<Button
+  		onPress={() => navigation.navigate('Search Recipes', {name: 'TEMP_USERNAME'})}
+  		title="Search Recipes"
+  		color="#50afff"
+  		accessibilityLabel="Search for recipes"/>
+	</View> 
+
+	<View style={[{ width: "40%", margin: 20 }]}>
+		<Button
+  		onPress={() => navigation.navigate('Add Recipes', {name: 'TEMP_USERNAME'})}
+  		title="Add Recipes"
+  		color="#50afff"
+  		accessibilityLabel="Add new recipes"/>
+	</View>
+
+    <View style={[{ width: "40%", margin: 20 }]}>
+		<Button
+  		onPress={() => navigation.navigate('Saved Recipes', {name: 'TEMP_USERNAME'})}
+  		title="Saved Recipes"
+  		color="#50afff"
+  		accessibilityLabel="Saved Recipes"/>
+	</View>
+
+	<View style={[{ width: "40%", margin: 20 }]}>
+		<Button
+  		onPress={() => navigation.navigate('Suggested Recipes', {name: 'TEMP_USERNAME'})}
+  		title="Suggested Recipes"
+  		color="#50afff"
+  		accessibilityLabel="Suggested recipes based on search"/>
+	</View>
+
+      </header>
+    </div>
+  );
+};
+
+export default HomeScreen;
