f4185331469a003edb6c33ef935d056d6285fab0
bradyawn <38081482+bradyawn@users.noreply.github.com>
2023-03-27 15:55:56 -0400

    Adding user context
    Instead of passing them and having a big mess, we're basically creating a global variable for user

---
+++ src/UserContext.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  Button,
+  View,
+  SafeAreaView,
+  Text,
+  Alert,
+} from 'react-native';
+
+//UserContext.js
+const UserContext = React.createContext();
+export default UserContext;
