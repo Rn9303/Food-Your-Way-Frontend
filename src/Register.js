d5297c7dd6d30ea6ed3999ba8489cb8e4929fa1f
Ricky Chu <rchu6120@gmail.com>
2023-03-30 18:47:51 -0400

    small changes to register form/touched it up

--- src/Register.js
+++ src/Register.js
@@ -13,19 +13,19 @@ function RegistrationForm() {
               </div>
               <div className="lastName">
                   <label className="form__label" for="lastName">Last Name </label>
-                  <input  type="text" name="" id="lastName"  className="form__input"placeholder="Last Name"/>
+                  <input className="form__input" type="text" id="lastName" placeholder="Last Name"/>
               </div>
               <div className="email">
                   <label className="form__label" for="email">Email </label>
-                  <input  type="email" id="email" className="form__input" placeholder="Email"/>
+                  <input className="form__input" type="email" id="email" placeholder="Email"/>
               </div>
               <div className="username">
-                  <label className="form__label" htmlFor="username">Username </label>
-                  <input type="username" id="username" className="form__input" placeholder="Username"/>
+                  <label className="form__label" for="username">Username </label>
+                  <input className="form__input" type="text" id="username" placeholder="Username"/>
               </div>
               <div className="password">
                   <label className="form__label" for="password">Password </label>
-                  <input className="form__input" type="password"  id="password" placeholder="Password"/>
+                  <input className="form__input" type="password" id="password" placeholder="Password"/>
               </div>
               <div className="confirm-password">
                   <label className="form__label" for="confirmPassword">Confirm Password </label>
